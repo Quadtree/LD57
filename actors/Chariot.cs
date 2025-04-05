cf3866978903d544516df7aad44a396ea4c9b066
Quadtree <quadtree@gmail.com>
2025-04-05 00:46:06 -0700

    U

--- actors/Chariot.cs
+++ actors/Chariot.cs
@@ -3,4 +3,17 @@ using Godot;
 
 public partial class Chariot : Node3D
 {
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+        var moveVector = new Vector2(0, 0);
+
+        if (Input.IsActionPressed("move_left")) moveVector.X = -1;
+        if (Input.IsActionPressed("move_right")) moveVector.X = 1;
+        if (Input.IsActionPressed("move_up")) moveVector.Y = 1;
+        if (Input.IsActionPressed("move_down")) moveVector.Y = -1;
+
+        this.
+    }
 }
