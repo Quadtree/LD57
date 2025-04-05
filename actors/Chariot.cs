699815efc085bb5f5c09f3158e79d7eee11deb04
Quadtree <quadtree@gmail.com>
2025-04-05 01:43:51 -0700

    U

--- actors/Chariot.cs
+++ actors/Chariot.cs
@@ -21,15 +21,24 @@ public partial class Chariot : Node3D
 
         var moveVector = new Vector2(0, 0);
 
+        var mbb = this.FindChildByName<Buoyancy>("MainBodyBuoyancy");
+        var oldB = mbb.Amount;
+
         if (Input.IsActionPressed("move_left")) moveVector.X = -1;
         if (Input.IsActionPressed("move_right")) moveVector.X = 1;
-        if (Input.IsActionPressed("move_up")) moveVector.Y = 1 / LeftRightRatio;
-        if (Input.IsActionPressed("move_down")) moveVector.Y = -1 / LeftRightRatio;
+        if (Input.IsActionPressed("move_up"))
+        {
+            moveVector.Y = 1 / LeftRightRatio;
+            mbb.Amount = Util.Clamp(mbb.Amount + ((float)delta * BuoyancyChangeRate), MinBuoyancy, MaxBuoyancy);
+        }
+        if (Input.IsActionPressed("move_down"))
+        {
+            moveVector.Y = -1 / LeftRightRatio;
+            mbb.Amount = Util.Clamp(mbb.Amount - ((float)delta * BuoyancyChangeRate), MinBuoyancy, MaxBuoyancy);
+        }
 
         this.FindChildByName<RigidBody3D>("SeaHorse").ApplyCentralForce(new Vector3(moveVector.X, moveVector.Y, 0) * 300);
 
-        var mbb = this.FindChildByName<Buoyancy>("MainBodyBuoyancy");
-        var oldB = mbb.Amount;
         if (Input.IsActionPressed("increase_buoyancy")) mbb.Amount = Util.Clamp(mbb.Amount + ((float)delta * BuoyancyChangeRate), MinBuoyancy, MaxBuoyancy);
         if (Input.IsActionPressed("decrease_buoyancy")) mbb.Amount = Util.Clamp(mbb.Amount - ((float)delta * BuoyancyChangeRate), MinBuoyancy, MaxBuoyancy);
         if (mbb.Amount != oldB)
