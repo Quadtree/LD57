6246d42bab7ba0946f333ec1b1e1b2efeed0f7ef
Quadtree <quadtree@gmail.com>
2025-04-05 12:04:17 -0700

    U

--- actors/AggressiveFish.cs
+++ actors/AggressiveFish.cs
@@ -3,4 +3,21 @@ using System;
 
 public partial class AggressiveFish : RigidBody3D
 {
+    [Export]
+    float Speed;
+
+    [Export]
+    float DamagePerHit;
+
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+        
+    }
 }
