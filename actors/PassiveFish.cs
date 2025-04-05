4690299dbbdc7345ce3c2d6ba2815c2f4b42f3af
Quadtree <quadtree@gmail.com>
2025-04-05 11:39:23 -0700

    U

--- actors/PassiveFish.cs
+++ actors/PassiveFish.cs
@@ -1,15 +1,24 @@
-using Godot;
 using System;
+using Godot;
 
 public partial class PassiveFish : RigidBody3D
 {
     [Export]
     public float Speed;
 
+    [Export]
+    public float Acceleration;
+
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
 
-        
+        var targetSpeed = (ToGlobal(Vector3.Forward) - GlobalPosition) * Speed;
+
+        var changeInSpeed = (targetSpeed - LinearVelocity).Normalized() * Acceleration;
+
+        GD.Print($"changeInSpeed={changeInSpeed}");
+
+        ApplyCentralForce(changeInSpeed);
     }
 }
