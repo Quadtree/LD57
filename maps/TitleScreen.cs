17d75195838bcc2c8a031e49342278720b77e5a6
Quadtree <quadtree@gmail.com>
2025-04-06 10:59:57 -0700

    U

--- maps/TitleScreen.cs
+++ maps/TitleScreen.cs
@@ -8,5 +8,11 @@ public partial class TitleScreen : Node3D
         base._Ready();
 
         GetTree().Paused = true;
+
+        for (var i = 0; i <= 4; ++i)
+        {
+            var li = i;
+            this.FindChildByName<Button>($"Level{i}Button").Pressed += () => { GetTree().Paused = false; GetTree().ChangeSceneToFile($"res://maps/Level{li}.tscn"); };
+        }
     }
 }
