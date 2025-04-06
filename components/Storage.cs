113eb95408025245ecb8e1eb131aeb266b05ba6c
Quadtree <quadtree@gmail.com>
2025-04-05 23:39:45 -0700

    U

--- components/Storage.cs
+++ components/Storage.cs
@@ -6,7 +6,6 @@ public partial class Storage : RigidBody3D
     public void EnterStorage(Grabbable g)
     {
         Mass += g.Mass;
-        // @TODO: Change buoyancy
 
         Chariot.KelpCollected += g.ValueInKelp;
         Chariot.SuperGemsCollected += g.ValueInSuperGems;
