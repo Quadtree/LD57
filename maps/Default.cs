e56bd538092675767798d12ce61974ef85ab8e73
Quadtree <quadtree@gmail.com>
2025-04-06 10:19:35 -0700

    U

--- maps/Default.cs
+++ maps/Default.cs
@@ -5,4 +5,29 @@ public partial class Default : Node3D
 {
     [Export]
     float StartingDepth;
+
+    float YAtStartingDepth;
+
+    public float? CurrentDepth
+    {
+        get
+        {
+            var chariot = this.FindChildByType<Chariot>();
+            if (chariot != null)
+            {
+                return chariot.MainBodyPos.Y - YAtStartingDepth + StartingDepth;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        YAtStartingDepth = this.FindChildByType<Chariot>().MainBodyPos.Y;
+    }
 }
