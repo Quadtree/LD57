32757ada564ad06316da1046edd02d96cbd029ba
Quadtree <quadtree@gmail.com>
2025-04-05 18:23:36 -0700

    U

--- actors/WaterJet.cs
+++ actors/WaterJet.cs
@@ -1,17 +1,29 @@
-using Godot;
 using System;
+using Godot;
 
 public partial class WaterJet : StaticBody3D
 {
+    [Export]
+    float Range;
+
+    [Export]
+    float MaxForce;
+
+    [Export]
+    float MinForce;
+
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
 
-        foreach (var it in GetTree().CurrentScene.FindChildrenByType<RigidBody3D>())
+        foreach (var it in this.FindChildByType<Area3D>().GetOverlappingBodies())
         {
-            var localPos = ToLocal(it.GlobalPosition);
+            if (it is RigidBody3D rb3d)
+            {
+                var rotator = new Transform3D(Basis, Vector3.Zero);
 
-            
+                rb3d.ApplyCentralForce(rotator * Vector3.Up * Mathf.Lerp(MinForce, MaxForce, rb3d.GlobalPosition.DistanceTo(GlobalPosition) / Range));
+            }
         }
     }
 }
