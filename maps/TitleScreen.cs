12d2fd479c2ea559a0c4626f25e9e2f9ec56e949
Quadtree <quadtree@gmail.com>
2025-04-06 14:49:44 -0700

    U

--- maps/TitleScreen.cs
+++ maps/TitleScreen.cs
@@ -17,6 +17,8 @@ public partial class TitleScreen : Node3D
                 var musicPlayer = new AudioStreamPlayer();
                 GetTree().Root.AddChild(musicPlayer);
                 musicPlayer.Stream = GD.Load<AudioStream>("res://midi/bgm.ogg");
+                musicPlayer.VolumeDb = -5;
+                musicPlayer.ProcessMode = ProcessModeEnum.Always;
                 musicPlayer.Play();
 
                 GetTree().Paused = false;
